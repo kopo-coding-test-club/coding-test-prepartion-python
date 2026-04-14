9fc24ac119aff2d66e49a3eaa12d61634b5fe4ca
bu2000 <bu2000@gmail.com>
2026-04-15 00:32:08 +0900

    fix: 12947_HarshadNumber 코드 수정

--- April/Second_Week/KimBuYeon/12947_Lv1_HarshadNumber.py
+++ April/Second_Week/KimBuYeon/12947_Lv1_HarshadNumber.py
@@ -1,2 +1,11 @@
-def solution(my_string, k):
-    return my_string * k
+def solution(x):
+    answer = False
+    r_num = x
+    s = 0
+    while x > 0:
+        s += x % 10
+        x //= 10
+    if r_num % int(s) == 0:
+        answer = True
+        
+    return answer
