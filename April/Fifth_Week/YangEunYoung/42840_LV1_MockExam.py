911c81b6cf3c629238a0a30500444f018cd0dbd2
yangtori0407 <114906941+yangtori0407@users.noreply.github.com>
2026-05-05 21:43:02 +0900

    DONE: 42840_MockExam.py

--- April/Fifth_Week/YangEunYoung/42840_LV1_MockExam.py
+++ April/Fifth_Week/YangEunYoung/42840_LV1_MockExam.py
@@ -4,7 +4,7 @@ def solution(answers):
     two = [2,1,2,3,2,4,2,5]
     three = [3,3,1,1,2,2,4,4,5,5]
     score = [0,0,0]
-    
+
     for i in range(len(answers)):
         if(one[i % len(one)] == answers[i]): 
             score[0] = score[0] + 1
