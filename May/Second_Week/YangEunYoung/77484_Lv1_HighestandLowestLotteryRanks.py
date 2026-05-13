c444c09e98af1d754eb207cfb92a6769270cb1db
yangtori0407 <114906941+yangtori0407@users.noreply.github.com>
2026-05-13 14:26:53 +0900

    DONE: 77484_HighestandLowestLotteryRanks.py

---
+++ May/Second_Week/YangEunYoung/77484_Lv1_HighestandLowestLotteryRanks.py
@@ -0,0 +1,30 @@
+def solution(lottos, win_nums):
+    answer = []
+    grade = [6, 5, 4, 3, 2]
+    first = 0;
+    last = 0;
+    
+    for win in win_nums:
+        for lotto in lottos:
+            if(win == lotto):
+                first += 1;
+                last += 1;
+
+    
+    for lotto in lottos:
+        if(lotto == 0):
+            first += 1;
+    
+    if(first <= 1):
+        answer.append(6)
+    else:
+        answer.append(grade.index(first) + 1)
+    if(last <= 1):
+        answer.append(6)
+    else:
+        answer.append(grade.index(last) + 1)
+    
+    
+    
+    
+    return answer
